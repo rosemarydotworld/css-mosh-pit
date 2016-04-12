db74633471e93f55668a45604dc2ecde683d5565
Liam Campbell <me@liamc.org>
2016-04-11 21:18:26 -0700

    Add a handy loading indicator

--- stage.js
+++ stage.js
@@ -1,10 +1,15 @@
 function init() {
   var mosh     = document.getElementById('mosh');
+  var stage    = document.getElementById('the-stage');
+  var loader   = document.getElementById('the-loader')
 
   var ref      = new Firebase('https://css-mosh-pit.firebaseio.com/');
   var headless = new Firepad.Headless(ref);
 
   ref.on('value', function(dataSnapshot) {
+    stage.setAttribute('hidden', 'false');
+    loader.setAttribute('hidden', 'true');
+
     headless.getText(function(styles) {
       mosh.innerText = styles;
     });
