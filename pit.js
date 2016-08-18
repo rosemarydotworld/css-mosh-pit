398a8b532ecb1a0a75b4ac866f6013fe1a3aa271
Liam Campbell <liam@muledesign.com>
2016-08-18 14:49:12 -0700

    Finally finish that ES6 fun

--- pit.js
+++ pit.js
@@ -1,49 +1,57 @@
-function init() {
-  //// Initialize Firebase.
-  var firepadRef = new Firebase('https://css-mosh-pit.firebaseio.com/');
-  //// Create ACE
+class Pit {
+  transformCSS(styles) { 
+    return this.cleanPseudoContent(this.cleanUrl(this.cleanWildcards(styles)))
+  }
+  
+  cleanPseudoContent(styles) {
+    const contentRegex = new RegExp('content:.+', 'im')
 
-  var editor = ace.edit("the-pit");
-  editor.setTheme("ace/theme/solarized_dark");
+    return styles.replace(contentRegex, "/* nope */")
+  }
 
-  var session = editor.getSession();
-  session.setTabSize(2);
-  session.setUseSoftTabs(true);
-  session.setUseWrapMode(true);
-  session.setUseWorker(false);
-  session.setMode("ace/mode/css");
+  cleanUrl(styles) {
+    const urlRegex = new RegExp('url\s*\((.+)\)', 'im')
 
-  //// Create Firepad.
-  var firepad = Firepad.fromACE(firepadRef, editor, {});
+    return styles.replace(urlRegex, "/* heck nope */")
+  }
 
-  firepad.on('synced', function(isSynced) {
+  cleanWildcards(styles) {
+    const wildcardRegex = new RegExp('.*\\*.*{', 'im')
+
+    return styles.replace(wildcardRegex, "/* nah */")
+  }
+}
+
+window.onload = () => {
+  // Initialize Firebase
+  const firepadRef = new Firebase('https://css-mosh-pit.firebaseio.com/')
+
+  // Create ACE
+  const editor = ace.edit("the-pit")
+  editor.setTheme("ace/theme/solarized_dark")
+
+  // Configure editor
+  const session = editor.getSession()
+  session.setTabSize(2)
+  session.setUseSoftTabs(true)
+  session.setUseWrapMode(true)
+  session.setUseWorker(false)
+  session.setMode("ace/mode/css")
+
+  // Create Firepad to sync the whole thing
+  const firepad = Firepad.fromACE(firepadRef, editor, {})
+
+  const pit = new Pit();
+
+  // On sync, do some transformations
+  firepad.on('synced', isSynced => {
     if(!isSynced) {
-      var styles = firepad.getText();
-      var preparedStyles = cleanWildcards(cleanPseudoContent(cleanUrl(styles)));
+      const styles = firepad.getText()
+      const preparedStyles = pit.transformCSS(styles)
 
       if(preparedStyles != styles) {
-        firepad.setText(preparedStyles);
+        firepad.setText(preparedStyles)
       }
     }
-  });
+  })
 }
-
-function cleanPseudoContent(styles) {
-  var contentRegex = new RegExp('content:.+', 'im');
-
-  return styles.replace(contentRegex, "/* nope */");
-}
-
-function cleanUrl(styles) {
-  var urlRegex = new RegExp('url\s*\((.+)\)', 'im');
-
-  return styles.replace(urlRegex, "/* heck nope */");
-}
-
-function cleanWildcards(styles) {
-  var wildcardRegex = new RegExp('.*\\*.*{', 'im');
-
-  return styles.replace(wildcardRegex, "/* nah */")
-}
-
-window.onload = init;
--- stage.js
+++ stage.js
@@ -1,19 +1,15 @@
-function init() {
-  var mosh     = document.getElementById('mosh');
-  var stage    = document.getElementById('the-stage');
-  var loader   = document.getElementById('the-loader')
+window.onload = () => {
+  const mosh     = document.getElementById('mosh');
+  const stage    = document.getElementById('the-stage');
+  const loader   = document.getElementById('the-loader')
 
-  var ref      = new Firebase('https://css-mosh-pit.firebaseio.com/');
-  var headless = new Firepad.Headless(ref);
+  const ref      = new Firebase('https://css-mosh-pit.firebaseio.com/');
+  const headless = new Firepad.Headless(ref);
 
-  ref.on('value', function(dataSnapshot) {
+  ref.on('value', dataSnapshot => {
     stage.setAttribute('hidden', 'false');
     loader.setAttribute('hidden', 'true');
 
-    headless.getText(function(styles) {
-      mosh.innerText = styles;
-    });
+    headless.getText(styles => { mosh.innerText = styles; });
   });
 }
-
-window.onload = init;
